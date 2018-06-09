6fc9516b35003014370f7b2ad7baf7f22f0c0980
Bruce Schubert <bruce@emxsys.com>
2018-06-09 14:27:05 -0700

    Setup build and test

--- src/Rothermel.js
+++ src/Rothermel.js
@@ -23,3 +23,6 @@
  */
 
 
+export default class Rothermel {
+    
+}
---
+++ test/Rothermel.test.js
@@ -0,0 +1,8 @@
+import Rothermel from '../src/Rothermel';
+
+
+describe('Rothermel', () => {
+  it('should exist', () => {
+    expect(Rothermel).toBeTruthy();
+  });
+});
